797969944c5b3a2496c047eee4e8fe948db131bc
Brian Lake <bnlake@gmail.com>
2023-03-04 17:40:04 -0600

    create our first tensors; print degrees

---
+++ tensorflow/intro.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+# Rank/Degree 0 tensor (also known as a scalar tensor) (Not contained within an array)
+string = tf.Variable("This is a string", tf.string)
+number = tf.Variable(12, tf.int16)
+floating = tf.Variable(3.14, tf.float16)
+print(tf.rank(number))
+
+# Rank/Degree 1 tensor
+tensor_rank1 = tf.Variable([1, 2, 4], tf.int16)
+print(tf.rank(tensor_rank1))
+
+# Rank/Degree 2 tensor (aka Matrice)
+tensor_rank2 = tf.Variable([[1, 2], [3, 4]], tf.int16)
+print(tf.rank(tensor_rank2));
